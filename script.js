073c8e99922debdd19de61a9b052f848043ee5e8
Meheraz321 <mdmeherazhosen44@gmail.com>
2025-04-21 10:30:10 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+console.log("Welcome to my portfolio site!");
+
+console.log("Welcome to my portfolio site!");
+
+// Dark Mode Toggle
+const toggleButton = document.getElementById('darkModeToggle');
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+});
